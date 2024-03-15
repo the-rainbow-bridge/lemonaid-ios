ecde76fa92e7a6ac2872c84c3edbef514b4c192d
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-03-15 17:09:07 -0400

    initial commit

---
+++ lemonaid/lemonaidApp.swift
@@ -0,0 +1,17 @@
+//
+//  lemonaidApp.swift
+//  lemonaid
+//
+//  Created by Eleni Papanicolas on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct lemonaidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
